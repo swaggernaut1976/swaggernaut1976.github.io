c05af33c3184dd6aa1761db23bbe46ea90a6d8e0
swaggernaut1976 <kebabstall@gmail.com>
2014-12-22 20:22:51 -0800

    clear

--- s.js
+++
@@ -1,8 +0,0 @@
-document.write('<span style="display: none;"><img id="albas" src="http://s.castalba.tv/r.php?ash='+ash+'&id='+id+'&cq='+cq+'" /></span>');
-
-	function albas() {
-		pingsts = document.getElementById('albas');
-		pingsts.src = "http://s.castalba.tv/r.php?ash="+ash+"&id="+id+"&cq="+cq+"&t=" + new Date().valueOf();
-	}
- 
-	setInterval("albas()", 270000);
